41ae00d36177666f3690abd804f6750df6193bfa
Amina Azzmouri <azzmouriamina@gmail.com>
2025-07-27 20:38:53 +0100

    feat: add ExampleForm to bookshelf/forms.py
    - Created a simple ExampleForm with name and email fields
    - Ensures compatibility with project requirements and tests

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -5,4 +5,9 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author', 'published_date']
+        
+        
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField()
 
