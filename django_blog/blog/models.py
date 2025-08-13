609d138cea2534d30425a73153ae212ab4e0dfd9
Amina Azzmouri <azzmouriamina@gmail.com>
2025-08-14 00:53:22 +0100

    Add published_date field to Post model to satisfy auto-checker requirements

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
-
+from django.utils import timezone  # Needed for default
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     date_posted = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(default=timezone.now)  # Add this field
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
